f784923e8ffc8c90ab1bb6f8b4d3fd7f61cb4e5c
aarondevtw <france7772001@icloud.com>
2017-01-22 03:30:09 +0800

    step6

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -15,4 +15,8 @@ export class HeroService {
       .then(() => this.getHeroes());
   }
 
+  getHero(id:number):Promise<Hero>{
+    return this.getHeroes().then(heroes => heroes.find(hero => hero.id===id));
+  }
+
 }
